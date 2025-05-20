8d53e94a2821ee199f30865cd6b88d79e8bfdce8
Bernardo Cezar <113948200+bercezar@users.noreply.github.com>
2025-05-20 15:20:14 -0300

    Update CarController.java

--- java/app.controller/CarController.java
+++ java/app.controller/CarController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +24,21 @@ public class CarController {
 	@Autowired
 	private CarService carService; 
 	
-	@PostMapping("/save") 
+	@PostMapping("/saveAll") 
 	public ResponseEntity<String> saveAll(@RequestBody List<Car> car){
 		try {
 			String message = this.carService.saveAll(car);
+			return ResponseEntity.ok(message);
+		}catch(Exception e){
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+		
+	}
+	
+	@PostMapping("/save") 
+	public ResponseEntity<String> save(@RequestBody Car car){
+		try {
+			String message = this.carService.save(car);
 			return ResponseEntity.ok(message);
 		}catch(Exception e){
 			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
@@ -44,4 +57,33 @@ public class CarController {
 		}
 		
 	}
+	
+	@GetMapping("/findAll")
+		public ResponseEntity<List<Car>> findAll(Car car){
+			try {
+				List<Car> cars = this.carService.findAll();
+				return ResponseEntity.ok(cars);
+				
+			}catch(Exception e){
+				return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+			}
+		}
+	
+	@DeleteMapping("/deleteById/{id}")
+		public ResponseEntity<String> deleteById(@PathVariable Long id){
+			String message = this.carService.deleteById(id);
+			return new ResponseEntity<String>(message, HttpStatus.OK);
+		}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Car> update(@PathVariable Long id, @RequestBody Car car) {
+	    try {
+	        Car updated = carService.update(id, car);
+	        return ResponseEntity.ok(updated);
+	    } catch (Exception e) {
+	        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+	    }
+	}
+
 }
+
